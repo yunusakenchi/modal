73f1274dbb9c3fda1105176fdaca21c79565a359
yunusakenchi <99495721+yunusakenchi@users.noreply.github.com>
2023-02-13 20:16:32 +0100

    Add files via upload

---
+++ modal/modal.js
@@ -0,0 +1,7 @@
+document.getElementById('open-modal').addEventListener('click', function () {
+    document.getElementById('overlay').style.display = "block"
+})
+
+document.getElementById('close-button').addEventListener('click', function() {
+    document.getElementById('overlay').style.display = "none"
+})
